bd92351ce06d3e4d594ebc06814f3d2c6b4adc1e
isahibjit <sahibjitsinghramgharia@gmail.com>
2025-05-31 15:22:31 +0530

    Add Intial Files with Navbar and Footer

---
+++ components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className='jakarta-font flex flex-col gap-5 p-12 mx-auto text-xl text-[#5C738A] max-w-[80%]'>
+        <div>
+            <ul className='flex justify-between mx-8 gap-8'>
+                <li className='cursor-pointer hover:text-[#44494e]'>About</li>
+                <li className='cursor-pointer hover:text-[#44494e]'>Contact</li>
+                <li className='cursor-pointer hover:text-[#44494e]'>Privacy Policy</li>
+                <li className='cursor-pointer hover:text-[#44494e]'>Terms</li>
+            </ul>
+        </div>
+        <div>
+            {/* social media icons */}
+            <ul className='flex justify-center gap-4 text-2xl'>
+                <li><FaXTwitter /></li>
+                <li><FaInstagram /></li>
+                <li><FaFacebookF /></li>
+            </ul>
+        </div>
+        <div>
+            <h1 className='text-center'>&copy; 2025 Fuel My Chai. All rights reserved.</h1>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="border border-x-0 border-t-0 border-slate-400 py-2">
+      <nav className="flex justify-between jakarta-font max-w-[95%] mx-auto items-center">
+        <Link href="/" className="flex items-center gap-2">
+        <img src="logo.gif" alt="chai logo" width={50} />
+          <p className="text-2xl jakarta-font-bold tracking-tighter">Fuel My Chai</p>
+        </Link>
+        <ul className="flex   gap-8 text-[#0F1417]  justify-center items-center">
+          <li className="text-xl font-medium hover:text-[#555151]">
+            <Link href="/explore">Explore</Link>
+          </li>
+          <li className="text-xl font-medium hover:text-[#555151]">
+            <Link href="/creators">Creators</Link>
+          </li>
+          <li className="text-xl  font-bold ">
+            <button className="btn-primary">
+              Log in
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
